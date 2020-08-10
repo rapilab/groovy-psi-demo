b6551126458663f322b767abd24c7ab9601cac96
Phodal Huang <h@phodal.com>
2020-08-10 10:29:38 +0800

    feat: try use import script for success build

--- src/main/kotlin/com/github/phodal/groovypsidemo/action/PsiMerge.kt
+++ src/main/kotlin/com/github/phodal/groovypsidemo/action/PsiMerge.kt
@@ -1,12 +1,23 @@
 package com.github.phodal.groovypsidemo.action
 
+import com.github.phodal.groovypsidemo.support.Constants
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.psi.PsiFileFactory
+import org.jetbrains.plugins.groovy.GroovyFileType
 import org.jetbrains.plugins.groovy.lang.psi.GroovyFile
 
 class PsiMerge : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project
-//        GroovyFile
+
+        val source = "".trim({ it <= ' ' })
+        val dest = "".trim({ it <= ' ' })
+
+        val templateBuildFile = PsiFileFactory.getInstance(project).createFileFromText(
+                Constants.FN_BUILD_GRADLE, GroovyFileType.GROOVY_FILE_TYPE, source) as GroovyFile
+        val existingBuildFile = PsiFileFactory.getInstance(project).createFileFromText(
+                Constants.FN_BUILD_GRADLE, GroovyFileType.GROOVY_FILE_TYPE, dest) as GroovyFile
+
     }
 }
