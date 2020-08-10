11c5d990e78b6c23bcd00990d2ee2755ee2019bb
Phodal Huang <h@phodal.com>
2020-08-10 10:22:53 +0800

    feat: make import success

--- src/main/kotlin/com/github/phodal/groovypsidemo/action/PsiMerge.kt
+++ src/main/kotlin/com/github/phodal/groovypsidemo/action/PsiMerge.kt
@@ -2,6 +2,7 @@ package com.github.phodal.groovypsidemo.action
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import org.jetbrains.plugins.groovy.lang.psi.GroovyFile
 
 class PsiMerge : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
